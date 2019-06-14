062cb47c2aabf9ae70a3ce1ccc257ee207685c9c
Paul Furnival <paulf@glpi.cspencerltd.co.uk>
2019-06-14 15:09:17 +0100

    Original code, very much in test mode

---
+++ hook.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/**
+ * Called when user click on Install - Needed
+ */
+function plugin_stockcontrol_install() { return true; }
+    
+/**
+ * Called when user click on Uninstall - Needed
+ */
+function plugin_stockcontrol_uninstall() { return true; }
